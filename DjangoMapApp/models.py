1bfc79afb9521aa73ff093fca8ab4ef978a19f47
eagobert <eagobert@hotmail.com>
2018-08-31 15:57:58 -0500

    add venue model

--- DjangoMapApp/models.py
+++ DjangoMapApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Venue(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    latitude = models.DecimalField(
+                max_digits=9, decimal_places=6, null=True, blank=True)
+
+    longitude = models.DecimalField(
+                max_digits=9, decimal_places=6, null=True, blank=True)
